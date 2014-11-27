9d4103084753db25566c061323b177bbb3ac1613
= <=>
2014-11-27 16:35:41 +0100

    full version

---
+++ modules/cbsSEAccess/language/it_it.lang.php
@@ -0,0 +1,20 @@
+<?php
+$mod_strings = array (
+//label of the distributor module
+    'cbsSEAccess' => 'Search Engine Access',
+    'cbsSEAccess' => 'Search Engine Access',
+    'sengineaccessno' => 'Search Engine Access No.',
+    'access_date' => 'Accesso Data',
+    'access_time' => 'Accesso Tempo',
+    'relid' => 'Relato a',
+    'access_url' => 'Accesso URL',
+    'access_web' => 'Accesso Web',
+    'organic' => 'Organico',
+
+    'LBL_CUSTOM_INFORMATION' => 'Informazioni Personalizzate',
+    'LBL_CBSSEACCESS_INFORMATION' => 'Search Engine Access Information',
+    'LBL_DESCRIPTION_INFORMATION' => 'Informazioni Descrizione',
+ //end
+ );
+
+?>
